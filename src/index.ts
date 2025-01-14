d177b259719d1c19ba7092226ddbe435f80f32ee
Daniel Herzig <dherzig@cloudflare.com>
2025-01-14 09:52:30 -0600

    NOJIRA: Update how headers are added

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,11 @@
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
         const res = await env.ASSETS.fetch(request)
-        return new Response(res.body, {
-            headers: {
-                ...res.headers,
-                // Cache for 7 days in user browsers
-                'Cache-Control': "max-age=604800",
-                'Content-Type': 'application/javascript',
-                'X-Served-By': "ninjapanel-navbar-worker",
-            }
-        });
+		const headers = new Headers(res.headers)
+		// Cache for 7 days in user browsers
+		headers.set("Cache-Control", "max-age=604800")
+		headers.set('Content-Type', 'application/javascript')
+		headers.append('X-Served-By', "ninjapanel-navbar-worker")
+		return new Response(res.body, { headers });
     }
 } satisfies ExportedHandler<Env>;
